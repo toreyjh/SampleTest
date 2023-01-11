75520913d2072de1a5f220f503bd968f33c1c3fa
Torey <Torey@TJH-DI5485>
2023-01-10 21:52:32 -0500

    trying again.

--- SampleTest/src/sample1/AiClass.java
+++ SampleTest/src/sample1/AiClass.java
@@ -4,6 +4,7 @@ public class AiClass {
 	
 	public static void main(String[] args) {
 		System.out.println("dd");
+		System.out.println("2");
 	}
 
 	public static String reverseString(String input) {
